6f2372fdee3e48bdabfcfa8748d322df6206d74e
tmchavez96 <tmchavez96@gmail.com>
2020-09-03 11:57:46 -0700

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { HeaderComponent } from './landing/header/header.component';
+import { BodyComponent } from './landing/body/body.component';
+import { FooterComponent } from './landing/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

9eea0fa07deb3e5024c9dff9407fc3554d28e177
tmchavez96 <tmchavez96@gmail.com>
2020-09-08 12:49:55 -0700

    first beta

--- src/app/landing/header/header.component.ts
+++ src/app/landing/header/header.component.ts
@@ -12,4 +12,28 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
+  navScroll(num) {
+    var id = ""
+    if(num == 1){
+      id="body-cont"
+    }else if(num == 2){
+      id="products-cont"
+    }else if(num == 3){
+      id = "podcast-cont"
+    }else{
+      id = "contact-cont"
+    }
+    console.log("going to scroll to elm id - " + id)
+   
+    let elms = document.getElementsByClassName(id)
+    let curElm = elms[0]
+    let curElmRect = curElm.getBoundingClientRect()
+    //window.scrollTo(0,curElmRect.top)
+   
+    elms[0].scrollIntoView({behavior:"smooth"})
+  }
+
+  log(){
+    console.log("hello")
+  }
 }
